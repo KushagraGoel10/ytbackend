e914f2143691464ec9f5ade7937a01f1f6a2bf74
Kushagra <goelkushagra2000@gmail.com>
2024-09-20 16:25:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// server.js or app.js
+
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios'); // Import axios for making HTTP requests
+const app = express();
+
+app.use(cors());
+
+app.get('/api/search', async (req, res) => {
+  const query = req.query.q;
+  try {
+    const response = await axios.get(`https://suggestqueries.google.com/complete/search`, {
+      params: {
+        client: 'firefox',
+        ds: 'yt',
+        q: query
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching data from Google API');
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log('Server is definitely running')
+});
